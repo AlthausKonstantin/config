88741a25e70566a447e0101dbf43accc2d17bca9
Konstantin Althaus <althaus.konstantin.95@gmail.com>
2025-03-28 17:38:50 +0100

    refactor: fonts

--- NixDarwin/darwin.nix
+++ NixDarwin/darwin.nix
@@ -36,15 +36,7 @@
   # ];
 
   fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
+    _0xproto
   ];
   networking.computerName = machineConfig.hostname;
   networking.hostName = machineConfig.hostname;
