57b8c2bb501c650a015b10c0ebcb3ec1535907f2
Konstantin Althaus <althaus.konstantin.95@gmail.com>
2025-03-30 19:23:38 +0200

    feat: mwe wezterm config

--- NixDarwin/home.nix
+++ NixDarwin/home.nix
@@ -65,6 +65,31 @@
           ls = "eza --all --icons=always --group-directories-first";
         };
       };
+      programs.wezterm = {
+        enable = true;
+        extraConfig = ''
+          -- Pull in the wezterm API
+          local wezterm = require("wezterm")
+
+          -- This will hold the configuration.
+          local config = wezterm.config_builder()
+
+          -- This is where you actually apply your config choices
+
+          config.font = wezterm.font("0xProto")
+          config.font_size = 19
+
+          config.enable_tab_bar = false
+
+          config.window_decorations = "RESIZE"
+
+          config.window_background_opacity = 0.95
+          config.macos_window_background_blur = 10
+
+          -- and finally, return the configuration to wezterm
+          return config
+        '';
+      };
       programs.eza = {
         enable = true;
         enableZshIntegration = true;
